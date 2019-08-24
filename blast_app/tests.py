658fe869965fb59c40ea2140fd3a0d90b5240d1e
Sebastian Theobald <setd@bio.dtu.dk>
2019-08-24 12:44:20 +0200

    Use tempdir to take care of cleanup

--- blast_app/tests.py
+++ blast_app/tests.py
@@ -2,12 +2,15 @@ import os
 import unittest
 
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from django.test import TestCase
 
 from .utils import seqtools as st
 
 # TODO: remove file, generally run in temp directory
+# Test BLAST in integration test
+# Test BLAST in unittest
 
 
 class SequenceToolsTest(unittest.TestCase):
@@ -21,15 +24,15 @@ class SequenceToolsTest(unittest.TestCase):
 
     def test_fasta_write(self):
         test_seq = "ATGATCGATCGATCGTGACTGATCGTGACGCAGTGCAT"
-        result_path = Path(
-            "blast_app/data").joinpath("unit_test_sequence.fasta")
-        st.write_bio_seq(test_seq,
-                         bio_id="test_id",
-                         seq_type="dna",
-                         bio_format="fasta",
-                         description="test_case",
-                         outfile=str(result_path)
-                         )
-        os.path.isfile(str(result_path))
+        with TemporaryDirectory() as tmppath:
+            outfile = Path(tmppath).joinpath("unittest_seq.fasta")
+            st.write_bio_seq(test_seq,
+                             bio_id="test_id",
+                             seq_type="dna",
+                             bio_format="fasta",
+                             description="test_case",
+                             outfile=str(outfile)
+                             )
+            os.path.isfile(str(outfile))
 
     # check that file exists
