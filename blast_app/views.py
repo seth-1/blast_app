3b6670af25d741c06f1ed21b7be1a6986c8f551e
Sebastian Theobald <setd@bio.dtu.dk>
2019-08-25 17:56:41 +0200

    removed template view, rather use react

--- blast_app/views.py
+++ blast_app/views.py
@@ -1,9 +1,3 @@
-from django.http import HttpResponseRedirect
-# from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-# from django.utils import timezone
-# from django.views import generic
-
 from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -14,35 +8,14 @@ from .tasks import prepare_blast
 
 
 class IndexView(generics.ListCreateAPIView):
-    # class IndexView(generic.ListView):
     queryset = BlastQuery.objects.all()
     serializer_class = BlastQuerySerializer
-    # template_name = 'blast_app/index.html'
-    # context_object_name = 'blast_list'
-
-    # def get_queryset(self):
-    #     """Return last blast results."""
-    #     return BlastQuery.objects.filter(
-    #         pub_date__lte=timezone.now()
-    #     ).order_by('-pub_date')[:10]
-
-
-class ResultsView(generics.RetrieveUpdateDestroyAPIView):
-    pass
-    # class ResultsView(generic.DetailView):
-    # queryset = BlastResult.objects.all() 1st try
-    # lookup_field = 'id'
-    # tmpset = BlastQuery.objects.all()
-    # queryset = tmpset.blastresult_set.all()
-    # serializer_class = BlastResultSerializer
-    # model = BlastQuery
-    # template_name = 'blast_app/results.html'
 
 
 @api_view(['POST'])
 def blast_request(request):
     dna_sequence = request.data
-    prepare_blast.delay(seq=dna_sequence,
-                        db='blast_app/data/proteins.fasta')
+    blast_res = prepare_blast.delay(seq=dna_sequence,
+                                    db='blast_app/data/proteins.fasta')
+    blast_res.wait(timeout=200, interval=0.5)
     return Response(status=status.HTTP_201_CREATED)
-    # return HttpResponseRedirect(reverse('blast_app:index'))
