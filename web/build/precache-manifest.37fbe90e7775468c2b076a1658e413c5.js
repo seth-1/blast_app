eeed1d3770f655e4bc6bba5a078a52e93f315fa9
Sebastian Theobald <setd@bio.dtu.dk>
2019-08-27 23:46:44 +0200

    add build

---
+++ web/build/precache-manifest.37fbe90e7775468c2b076a1658e413c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a45c338e31fc9abe9c87e5f1b63032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c220340308dda282a7a9",
+    "url": "/static/css/main.5c2ca7bd.chunk.css"
+  },
+  {
+    "revision": "d3c3d36fa9a858a78cc7",
+    "url": "/static/js/2.f90e8906.chunk.js"
+  },
+  {
+    "revision": "c220340308dda282a7a9",
+    "url": "/static/js/main.cef0f6f3.chunk.js"
+  },
+  {
+    "revision": "610b963a08a7f7d872c9",
+    "url": "/static/js/runtime~main.066cd76b.js"
+  }
+]);
---
+++ web/build/precache-manifest.9488baa361d5dd42a451e4a0b230a118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c98bb6fb89fd4a946d9d19c217cc72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7573338041ed483a22e",
+    "url": "/static/css/main.5c2ca7bd.chunk.css"
+  },
+  {
+    "revision": "d3c3d36fa9a858a78cc7",
+    "url": "/static/js/2.f90e8906.chunk.js"
+  },
+  {
+    "revision": "c7573338041ed483a22e",
+    "url": "/static/js/main.826ca3ae.chunk.js"
+  },
+  {
+    "revision": "610b963a08a7f7d872c9",
+    "url": "/static/js/runtime~main.066cd76b.js"
+  }
+]);
